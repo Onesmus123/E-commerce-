d455ab5ffe9d7e4113d83421e9284613be3eec6b
Onesmus Mutyauvyu <89151741+Onesmus123@users.noreply.github.com>
2024-08-22 11:55:10 +0300

    Update NewCollections.jsx

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -7,7 +7,7 @@ const NewCollections = () => {
   const [new_collection,setNew_Collection] = useState([]);
 
   useEffect(()=>{
-    fetch('http://localhost:4000/newCollections')
+    fetch('https://e-commerce-backend-37cn.onrender.com/newCollections')
     .then((response)=>response.json())
     .then((data)=>setNew_Collection(data));
   },[])
