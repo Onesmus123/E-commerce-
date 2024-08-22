9115dee94e5e1825c33aedcc51e58fa2a405b2f8
Onesmus Mutyauvyu <89151741+Onesmus123@users.noreply.github.com>
2024-08-22 11:45:58 +0300

    Update LoginSignUp.jsx

--- frontend/src/Pages/LoginSignUp.jsx
+++ frontend/src/Pages/LoginSignUp.jsx
@@ -17,7 +17,7 @@ const LoginSignUp = () => {
   const login = async () =>{
     console.log("Login Function Executed", formData);
     let responseData;
-    await fetch('http://localhost:4000/login',{
+    await fetch('https://e-commerce-backend-37cn.onrender.com/login',{
       method: 'POST',
       headers:{
         Accept:'application/form-data',
@@ -38,7 +38,7 @@ const LoginSignUp = () => {
   const signup = async () =>{
     console.log("Signup Function Executed",formData);
     let responseData;
-    await fetch('http://localhost:4000/signup',{
+    await fetch('https://e-commerce-backend-37cn.onrender.com/signup',{
       method: 'POST',
       headers:{
         Accept:'application/form-data',
